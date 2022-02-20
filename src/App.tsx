b4cf9c7157241cb1e883e2a72570072343c525c6
carlos <carls.mrz@gmail.com>
2022-02-20 00:32:41 -0500

    final

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,9 @@
 import { Counter } from './bases/Counter';
 import { CounterBy } from './bases/CounterBy';
 import { CounterEffect } from './bases/CounterEffect';
+import { CounterHook } from './bases/CounterHook';
+import { CounterReducerComponent } from './counter-reducer/CounterReducer';
+//import { CounterReducerComponent } from './bases/CounterReducer';
 
 function App() {
 	return (
@@ -10,6 +13,8 @@ function App() {
 			<Counter initialValue={5}/>
 			<CounterBy initialValue={15}/>
 			<CounterEffect/>
+			<CounterHook/>
+			<CounterReducerComponent/>
 		</>
 	);
 }
